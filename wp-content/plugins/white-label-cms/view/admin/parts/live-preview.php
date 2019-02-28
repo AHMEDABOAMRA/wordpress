1806fd9f3bfe4c35dff5178d929f314cd8d1f80b
AHMEDABOAMRA <45484644+AHMEDABOAMRA@users.noreply.github.com>
2019-03-01 00:05:23 +0100

    jj

--- wp-content/plugins/white-label-cms/view/admin/parts/live-preview.php
+++
@@ -1,6 +0,0 @@
-<div class="lightbox short-animate" id="wlcms-preview-content">
-    
-</div>
-<div id="lightbox-controls" class="short-animate">
-    <a id="close-lightbox" class="long-animate" href="#">Close Preview</a>
-</div>
